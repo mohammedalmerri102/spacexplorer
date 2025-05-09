27fdfd363ada28ba0fc6d8584a4f571e70f1d3c1
Mohammed Almeri <mohammedalmerri102@outlook.com>
2025-05-09 17:29:51 +0500

    add asteriods

--- game.c
+++ game.c
@@ -12,6 +12,7 @@ void init_game(struct gamestate* g) {
     
     g->fuel_level = 100;
     g->points = 0;
+    g->num_asteroids = MAX_ASTEROIDS;
     
     for(int i = 0; i < GRID_SIZE; i++) {
         for(int j = 0; j < GRID_SIZE; j++) {
@@ -21,6 +22,7 @@ void init_game(struct gamestate* g) {
     
     place_player(g);
     place_junk(g);
+    place_asteroids(g);
 }
 
 // shows the grid on screen
@@ -54,7 +56,7 @@ void place_player(struct gamestate* g) {
     g->grid[g->player_y][g->player_x] = PLAYER;
 }
 
-// adds random junk to the grid
+// adds junk to the grid
 void place_junk(struct gamestate* g) {
     int num_junk = 10;  // how much junk to add
     
@@ -69,17 +71,105 @@ void place_junk(struct gamestate* g) {
     }
 }
 
+// place asteroids on left side
+void place_asteroids(struct gamestate* g) {
+    for(int i = 0; i < g->num_asteroids; i++) {
+        g->asteroid_x[i] = 0;
+        g->asteroid_y[i] = rand() % GRID_SIZE;
+        g->grid[g->asteroid_y[i]][g->asteroid_x[i]] = ASTEROID;
+    }
+}
+
+// move all asteroids
+void move_asteroids(struct gamestate* g) {
+    for(int i = 0; i < g->num_asteroids; i++) {
+        // clear old position
+        g->grid[g->asteroid_y[i]][g->asteroid_x[i]] = EMPTY;
+        
+        // move right
+        g->asteroid_x[i]++;
+        if(g->asteroid_x[i] >= GRID_SIZE) {
+            g->asteroid_x[i] = 0;
+            g->asteroid_y[i] = rand() % GRID_SIZE;
+        }
+        
+        // place asteroid in new position
+        g->grid[g->asteroid_y[i]][g->asteroid_x[i]] = ASTEROID;
+    }
+}
+
+// check if player hit asteroid
+int check_collision(struct gamestate* g) {
+    for(int i = 0; i < g->num_asteroids; i++) {
+        if(g->player_x == g->asteroid_x[i] && g->player_y == g->asteroid_y[i]) {
+            return 1;  // collision!
+        }
+    }
+    return 0;  // no collision
+}
+
+// move player
+void move_player(struct gamestate* g, char direction) {
+    // clear old position
+    g->grid[g->player_y][g->player_x] = EMPTY;
+    
+    // update position based on direction
+    switch(direction) {
+        case 'w':  // up
+            if(g->player_y > 0) g->player_y--;
+            break;
+        case 's':  // down
+            if(g->player_y < GRID_SIZE-1) g->player_y++;
+            break;
+        case 'a':  // left
+            if(g->player_x > 0) g->player_x--;
+            break;
+        case 'd':  // right
+            if(g->player_x < GRID_SIZE-1) g->player_x++;
+            break;
+    }
+    
+    // place player in new position
+    g->grid[g->player_y][g->player_x] = PLAYER;
+}
+
 int main() {
     struct gamestate g;
+    char move;
     
     printf("Welcome to SpaceXplorer!\n");
     printf("Starting game...\n");
     
     init_game(&g);
-    print_grid(&g);
     
-    printf("\nGame ready! P is player, J is junk\n");
-    printf("Fuel: %d\n", g.fuel_level);
+    while(1) {
+        print_grid(&g);
+        printf("\nGame ready! P is player, J is junk, A is asteroid\n");
+        printf("Fuel: %d\n", g.fuel_level);
+        printf("Enter move (w=up, s=down, a=left, d=right, q=quit): ");
+        
+        scanf(" %c", &move);
+        
+        if(move == 'q') break;
+        
+        // move player
+        move_player(&g, move);
+        
+        // check for collision
+        if(check_collision(&g)) {
+            printf("\nGame Over! You hit an asteroid!\n");
+            break;
+        }
+        
+        // move asteroids
+        move_asteroids(&g);
+        
+        // check for collision after asteroid move
+        if(check_collision(&g)) {
+            printf("\nGame Over! An asteroid hit you!\n");
+            break;
+        }
+    }
     
     return 0;
 } 
--- game.h
+++ game.h
@@ -1,22 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
 
-// grid size needs to be at least 18x18
+// grid size
 #define GRID_SIZE 18
 
-// stuff that can be in the grid
+// Elemtns  in the grid
 #define EMPTY ' '
 #define PLAYER 'P'
 #define JUNK 'J'
 #define ASTEROID 'A'
+
+// max number of asteroids
+#define MAX_ASTEROIDS 3
 
 // keeps track of everything in the game
 struct gamestate {
     char grid[GRID_SIZE][GRID_SIZE];  // the board
     int player_x;                      // where player is
     int player_y;                      // player y pos
-    int fuel_level;                    // how much fuel left
+    int fuel_level;                    // fuel level
     int points;                        // score
+    int asteroid_x[MAX_ASTEROIDS];    // array of asteroid positions
+    int asteroid_y[MAX_ASTEROIDS];
+    int num_asteroids;                // current number of asteroids
 };
 
 // functions we need
@@ -24,5 +30,8 @@ void init_game(struct gamestate* g);
 void print_grid(struct gamestate* g);
 void place_player(struct gamestate* g);
 void place_junk(struct gamestate* g);
+void place_asteroids(struct gamestate* g);
+void move_asteroids(struct gamestate* g);
+int check_collision(struct gamestate* g);
 
 #endif 
